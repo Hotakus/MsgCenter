e0e3ff759b89b73247f392cd789ca9f2886a500e
Hotakus <553070703@qq.com>
2021-11-02 02:54:01 +0800

    feat(conf) add debug output alternative option

--- conf.h
+++ conf.h
@@ -7,12 +7,15 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
-
 typedef std::string String;
-
 using namespace std;
 
+#if MSG_CENTER_DEBUG == 1
 #define MSG_PRINT(tag, fmt, ...)    printf(fmt"\r\n", ##__VA_ARGS__)
+#else
+#define MSG_PRINT(tag, fmt, ...)
+#endif
+
 #define MSG_ALLOC                   malloc
 #define MSG_REALLOC                 realloc
 #define MSG_FREE                    free
@@ -25,8 +28,13 @@ using namespace std;
 #define HGM_DEBUG 1
 #include "../../HgmLogUtil.h"
 
-#define MSG_PRINT       hgm_log_e
+#if MSG_CENTER_DEBUG == 1
+#define MSG_PRINT    hgm_log_e
+#else
+#define MSG_PRINT(tag, fmt, ...)
+#endif
 
+#define MSG_PRINT       hgm_log_e
 #define MSG_ALLOC       hotakusAlloc
 #define MSG_REALLOC     hotakusRealloc
 #define MSG_FREE        hotakusFree
